1ad23dffc5547b29aad241d7c101d6ca14b5b19d
Eleazar Ortega <eleazar.ortega@lilab.pe>
2022-04-08 20:18:23 -0400

    Imagenes redimensionadas

--- src/app.module.ts
+++ src/app.module.ts
@@ -25,7 +25,7 @@ import { ConfigModule } from '@nestjs/config';
 @Module({
   imports: [
     ConfigModule.forRoot({
-      envFilePath: '.env',
+      envFilePath: process.env.NODE_ENV  == 'local'? '.local.env': '.env',
       isGlobal: true,
     }),
     MulterModule.register({
