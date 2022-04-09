81fde7277d7fe8feabea2b4f731ca3797befc81f
Eleazar Ortega <eleazar.ortega@lilab.pe>
2022-04-09 14:41:39 -0400

    integrado sistema para resolucion de conexion de BD

--- src/database/database.providers.ts
+++ src/database/database.providers.ts
@@ -1,9 +1,21 @@
 import * as mongoose from 'mongoose';
+console.log(process.env.NODE_ENV)
+
+const localConnection =   {
+  provide: 'DATABASE_CONNECTION',
+  useFactory: (): Promise<typeof mongoose> =>
+    mongoose.connect(`mongodb://${process.env.DATABASE_USERNAME}:${process.env.DATABASE_PASSWORD}@${process.env.DATABASE_HOST}/${process.env.DATABASE_NAME}?authSource=admin&readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false`),
+}
+
+
+const productionConnection =   {
+  provide: 'DATABASE_CONNECTION',
+  useFactory: (): Promise<typeof mongoose> =>
+    mongoose.connect(`mongodb://${process.env.DATABASE_USERNAME}:${process.env.DATABASE_PASSWORD}@${process.env.DATABASE_HOST}/${process.env.DATABASE_NAME}?authSource=admin&replicaSet=atlas-ialgos-shard-0&w=majority&readPreference=primary&appname=MongoDB%20Compass&retryWrites=true&ssl=true`),
+}
+
+const isLocalEnvironment = process.env.NODE_ENV === 'local'
 
 export const databaseProviders = [
-  {
-    provide: 'DATABASE_CONNECTION',
-    useFactory: (): Promise<typeof mongoose> =>
-      mongoose.connect(`mongodb://${process.env.DATABASE_USERNAME}:${process.env.DATABASE_PASSWORD}@${process.env.DATABASE_HOST}/${process.env.DATABASE_NAME}?authSource=admin&readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false`),
-  },
+  isLocalEnvironment ? localConnection : productionConnection
 ];
